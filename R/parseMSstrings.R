39dbc548b72c2af4848b931c6d4e0131ae5d9a0d
katopotat0 <katekru@gmail.com>
2026-02-17 18:12:14 -0500

    print

--- R/parseMSstrings.R
+++ R/parseMSstrings.R
@@ -1,18 +1,33 @@
 # parseMSstrings.R
 # utilities for parsing MS strings
 
-library(Biostrings) # for `AAStringSet` - BiocManager::install('Biostrings')
-library(dplyr) # for `tibble`
-library(purrr) # for `map` and `map_chr`
-library(stringr) # for `str_extract_all`, `str_replace`, `str_replace_all`, and `str_split`
+
+
+#' parse_mods
+#' Extracts data from an annotated peptide sequence from Spectronaut
+#'
+#' @param seqs A character vector of peptide sequences
+#' @param format A character value specifying the format of seqs (not currently implemented-function expects Spectronaut)
+#'
+#'@returns A `protein_mods` object
+#'@export
+#'@importFrom Biostrings AAStringSet
+#'@importFrom dplyr tibble
+#'@importFrom purrr map map_chr
+#'@importFrom stringr str_extract_all str_replace_all str_split
+
+
+
+
+
 
 parse_mods <- function(seqs, format = 'Spectronaut')
 {
   # remove '_'
   seqs <- seqs |>
     str_replace_all('_', '')
-  
-  
+
+
   # locate modification(s)
   mods <- str_extract_all(seqs, "\\[.*?\\]") |>
     map(~ .x |>
@@ -20,30 +35,30 @@ parse_mods <- function(seqs, format = 'Spectronaut')
           str_replace(fixed("]"), '') |>
           str_replace("\\(.*?\\)", '') |>
           trimws())
-  
-  
+
+
   # remove modifications from seqs
   seqs <- str_split(seqs, "\\[.*?\\]")
-  
-  
+
+
   # find where the mods are at
-  mods_at <- map(seqs, ~ 
+  mods_at <- map(seqs, ~
                    {
                      retval <- nchar(.x) |>
                        cumsum()
-                     
+
                      retval[-length(retval)] # this is the end of the peptide, not a modification
                    })
-  
-  
+
+
   # return the data.frame
   retval <- tibble(sequence = map_chr(seqs, ~ paste(.x, collapse = '')) |>
                      AAStringSet() |>
                      as.list(),
                    mods = mods,
                    mods_at = mods_at)
-  
+
   class(retval) <- c('protein_mods', class(retval))
-  
+
   return(retval)
 }
